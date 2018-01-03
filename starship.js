8e827e9b9a806affbdf4737bbf233bb23e48f478
Briggs Elsperger <I3uckwheat@gmail.com>
2018-01-02 18:16:24 -0600

    adds run function

--- starship.js
+++ starship.js
@@ -1,1 +1,16 @@
+function run(genFunc){
+  const genObject = genFunc();
 
+  function iterate(iteration){
+    if(iteration.done) return Promise.resolve(iteration.value);
+    return Promise.resolve(iteration.value)
+              .then(result => iterate(genObject.next(result)))
+              .catch(error => iterate(genObject.throw(error)));
+  }
+
+  try {
+    return iterate(genObject.next());
+  } catch(ex) {
+    return Promise.reject(ex);
+  }
+}
