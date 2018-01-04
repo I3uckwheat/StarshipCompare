da901ec9f45017a1e19edb554588f39e0c047a73
Briggs Elsperger <I3uckwheat@gmail.com>
2018-01-03 19:11:45 -0600

    refactor

--- starship.js
+++ starship.js
@@ -31,22 +31,22 @@ function *compareStarships(){
   fillStarshipTable(ship1Obj, ship2Obj);
 }
 
+
 function fillStarshipTable(ship1, ship2){
+  const ships = [ship1, ship2]
   const names = document.getElementById("name").children;
   const cost = document.getElementById("cost").children;
   const speed = document.getElementById("speed").children;
   const cargo = document.getElementById("cargo").children;
   const passengers = document.getElementById("passengers").children;
 
-    names[1].textContent = ship1.name
-    cost[1].textContent = ship1.cost_in_credits;
-    speed[1].textContent = ship1.max_atmosphering_speed;
-    cargo[1].textContent = ship1.cargo_capacity;
-    passengers[1].textContent = ship1.passengers;
+  ships.forEach((ship, i) => {
+    const j = i + 1;
 
-    names[2].textContent = ship2.name;
-    cost[2].textContent = ship2.cost_in_credits;
-    speed[2].textContent = ship2.max_atmosphering_speed;
-    cargo[2].textContent = ship2.cargo_capacity;
-    passengers[2].textContent = ship2.passengers;
+    names[j].textContent      = ship.name;
+    cost[j].textContent       = ship.cost_in_credits;
+    speed[j].textContent      = ship.max_atmosphering_speed;
+    cargo[j].textContent      = ship.cargo_capacity;
+    passengers[j].textContent = ship.passengers;
+  })
 }
